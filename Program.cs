9f07007f97713885a97d711f73cc277b9cb55be9
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-19 17:10:24 +0300

    Задача4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("Максимальное число: " + num1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + num3);
+    }
+}
+else if (num2 > num3)
+{
+    Console.WriteLine("Максимальное число: " + num2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + num3);
+}
